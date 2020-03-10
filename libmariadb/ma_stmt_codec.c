cpp-for-everything/mariadb-connector-c

commit 2e804f0badce962057254841efae80715d2bf975

Fix for CONC-452 and CONC-453:
Various coverity scan fixes, including CONC-452 and CONC-453.
Special thanks to Lukas Javorsky for fixing numerous covscan
issues (This patch includes part of his pull request #126).

Coverity scan build was using the following cmake parameters:
-WITH_EXTERNAL_ZLIB=ON -DWITH_UNIT_TESTS=OFF.

CWE-416 (use after free) in dtoa.c (from netlib) is still open.

libmariadb/ma_stmt_codec.c
@@ -49,6 +49,7 @@
 #include <mariadb_ctype.h>
 #include "mysql.h"
 #include <math.h> /* ceil() */
+#include <limits.h>
 
 #ifdef WIN32
 #include <malloc.h>
@@ -71,6 +72,8 @@
 #define LONGLONG_MIN    ((long long) 0x8000000000000000LL)
 #define LONGLONG_MAX    ((long long) 0x7FFFFFFFFFFFFFFFLL)
 #endif
+
+#define MAX_DBL_STR 3 + DBL_MANT_DIG - DBL_MIN_EXP
 
 #if defined(HAVE_LONG_LONG) && !defined(ULONGLONG_MAX)
 /* First check for ANSI C99 definition: */
@@ -254,19 +257,25 @@ static unsigned long long my_atoull(const char *str, const char *end_str, int *e
 double my_atod(const char *number, const char *end, int *error)
 {
   double val= 0.0;
-  char buffer[255];
+  char buffer[MAX_DBL_STR + 1];
   int len= (int)(end - number);
 
-  if (len > 254) 
-    *error= 1;
+  *error= errno= 0;
 
-  len= MIN(len, 254);
-  memcpy(&buffer, number, len);
+  if (len > MAX_DBL_STR)
+  {
+    *error= 1;
+    len= MAX_DBL_STR;
+  }
+
+  memcpy(buffer, number, len);
   buffer[len]= '\0';
 
   val= strtod(buffer, NULL);
-/*  if (!*error)
-    *error= errno; */
+
+  if (errno)
+    *error= errno;
+
   return val;
 }
 
@@ -570,21 +579,26 @@ static void convert_froma_string(MYSQL_BIND *r_param, char *buffer, size_t len)
     case MYSQL_TYPE_NEWDECIMAL:
     default:
     {
-      char *start= buffer + r_param->offset; /* stmt_fetch_column sets offset */
-      char *end= buffer + len;
-      size_t copylen= 0;
-
-      if (start < end)
+      if (len > r_param->offset)
       {
-        copylen= end - start;
-        if (r_param->buffer_length)
-          memcpy(r_param->buffer, start, MIN(copylen, r_param->buffer_length));
-      }
-      if (copylen < r_param->buffer_length)
-        ((char *)r_param->buffer)[copylen]= 0;
-      *r_param->error= (copylen > r_param->buffer_length);
+        char *start= buffer + r_param->offset; /* stmt_fetch_column sets offset */
+        char *end= buffer + len;
+        size_t copylen= 0;
 
-      *r_param->length= (ulong)len; 
+        if (start < end)
+        {
+          copylen= end - start;
+          if (r_param->buffer_length)
+            memcpy(r_param->buffer, start, MIN(copylen, r_param->buffer_length));
+        }
+        if (copylen < r_param->buffer_length)
+          ((char *)r_param->buffer)[copylen]= 0;
+        *r_param->error= (copylen > r_param->buffer_length);
+
+      }
+      else
+        *r_param->error= 1;
+      *r_param->length= (ulong)len;
     }
     break;
   }
@@ -657,7 +671,7 @@ static void convert_from_long(MYSQL_BIND *r_param, const MYSQL_FIELD *field, lon
         if (display_width < r_param->buffer_length)
         {
           ma_bmove_upp(buffer + display_width, buffer + len, len);
-          /* coverity [bad_memset] */
+          /* coverity[bad_memset] */
           memset((void*) buffer, (int) '0', display_width - len);
           len= display_width;
         }
@@ -880,7 +894,7 @@ static void convert_from_float(MYSQL_BIND *r_param, const MYSQL_FIELD *field, fl
         if (field->length < length || field->length > MAX_DOUBLE_STRING_REP_LENGTH - 1)
           break;
         ma_bmove_upp(buff + field->length, buff + length, length);
-        /* coverity [bad_memset] */
+        /* coverity[bad_memset] */
         memset((void*) buff, (int) '0', field->length - length);
         length= field->length;
       }
@@ -1103,74 +1117,68 @@ void ps_fetch_datetime(MYSQL_BIND *r_param, const MYSQL_FIELD * field,
   unsigned int len= net_field_length(row);
 
   switch (r_param->buffer_type) {
-  case MYSQL_TYPE_DATETIME:
-  case MYSQL_TYPE_TIMESTAMP:
-    convert_to_datetime(t, row, len, field->type);
-    break;
-  case MYSQL_TYPE_DATE:
-    convert_to_datetime(t, row, len, field->type);
-    break;
-  case MYSQL_TYPE_TIME:
-    convert_to_datetime(t, row, len, field->type);
-    t->year= t->day= t->month= 0;
-    break;
-  case MYSQL_TYPE_YEAR:
-  {
-    MYSQL_TIME tm;
-    convert_to_datetime(&tm, row, len, field->type);
-    shortstore(r_param->buffer, tm.year);
-    break;
-  }
-  default: 
-  {
-    char dtbuffer[60];
-    MYSQL_TIME tm;
-    size_t length;
-    convert_to_datetime(&tm, row, len, field->type);
- /*   
-    if (tm.time_type== MYSQL_TIMESTAMP_TIME && tm.day)
-    {
-      tm.hour+= tm.day * 24;
-      tm.day=0;
-    }
-*/
-    switch(field->type) {
-    case MYSQL_TYPE_DATE:
-      length= sprintf(dtbuffer, "%04u-%02u-%02u", tm.year, tm.month, tm.day);
-      break;
-    case MYSQL_TYPE_TIME:
-      length= sprintf(dtbuffer, "%s%02u:%02u:%02u", (tm.neg ? "-" : ""), tm.hour, tm.minute, tm.second);
-      if (field->decimals && field->decimals <= 6)
-      {
-        char ms[8];
-        sprintf(ms, ".%06lu", tm.second_part);
-        if (field->decimals < 6)
-          ms[field->decimals + 1]= 0;
-        length+= strlen(ms);
-        strcat(dtbuffer, ms);
-      }
-      break;
     case MYSQL_TYPE_DATETIME:
     case MYSQL_TYPE_TIMESTAMP:
-      length= sprintf(dtbuffer, "%04u-%02u-%02u %02u:%02u:%02u", tm.year, tm.month, tm.day, tm.hour, tm.minute, tm.second);
-      if (field->decimals && field->decimals <= 6)
-      {
-        char ms[8];
-        sprintf(ms, ".%06lu", tm.second_part);
-        if (field->decimals < 6)
-          ms[field->decimals + 1]= 0;
-        length+= strlen(ms);
-        strcat(dtbuffer, ms);
-      }
+      convert_to_datetime(t, row, len, field->type);
       break;
-    default:
-      dtbuffer[0]= 0;
-      length= 0;
-    break;
+    case MYSQL_TYPE_DATE:
+      convert_to_datetime(t, row, len, field->type);
+      break;
+    case MYSQL_TYPE_TIME:
+      convert_to_datetime(t, row, len, field->type);
+      t->year= t->day= t->month= 0;
+      break;
+    case MYSQL_TYPE_YEAR:
+    {
+      MYSQL_TIME tm;
+      convert_to_datetime(&tm, row, len, field->type);
+      shortstore(r_param->buffer, tm.year);
+      break;
     }
-    convert_froma_string(r_param, dtbuffer, length);
-    break;
-  }
+    default: 
+    {
+      char dtbuffer[60];
+      MYSQL_TIME tm;
+      size_t length;
+      convert_to_datetime(&tm, row, len, field->type);
+
+      switch(field->type) {
+      case MYSQL_TYPE_DATE:
+        length= sprintf(dtbuffer, "%04u-%02u-%02u", tm.year, tm.month, tm.day);
+        break;
+      case MYSQL_TYPE_TIME:
+        length= sprintf(dtbuffer, "%s%02u:%02u:%02u", (tm.neg ? "-" : ""), tm.hour, tm.minute, tm.second);
+        if (field->decimals && field->decimals <= 6)
+        {
+          char ms[8];
+          sprintf(ms, ".%06lu", tm.second_part);
+          if (field->decimals < 6)
+            ms[field->decimals + 1]= 0;
+          length+= strlen(ms);
+          strcat(dtbuffer, ms);
+        }
+        break;
+      case MYSQL_TYPE_DATETIME:
+      case MYSQL_TYPE_TIMESTAMP:
+        length= sprintf(dtbuffer, "%04u-%02u-%02u %02u:%02u:%02u", tm.year, tm.month, tm.day, tm.hour, tm.minute, tm.second);
+        if (field->decimals && field->decimals <= 6)
+        {
+          char ms[8];
+          sprintf(ms, ".%06lu", tm.second_part);
+          if (field->decimals < 6)
+            ms[field->decimals + 1]= 0;
+          length+= strlen(ms);
+          strcat(dtbuffer, ms);
+        }
+        break;
+      default:
+        dtbuffer[0]= 0;
+        length= 0;
+        break;
+      }
+      convert_froma_string(r_param, dtbuffer, length);
+      break;
+    }
   }
   (*row) += len;
 }
@@ -1353,3 +1361,4 @@ void mysql_init_ps_subsystem(void)
  * vim600: noet sw=4 ts=4 fdm=marker
  * vim<600: noet sw=4 ts=4
  */
+

include/errmsg.h
@@ -103,5 +103,5 @@ extern const char *mariadb_client_errors[];	/* Error messages */
 #define CR_VERSION_MISMATCH 5008
 /* Always last, if you add new error codes please update the
    value for CR_MARIADB_LAST_ERROR */
-#define CR_MARIADB_LAST_ERROR CR_INVALID_STMT
+#define CR_MARIADB_LAST_ERROR CR_VERSION_MISMATCH
 #endif

libmariadb/ma_dtoa.c
@@ -1121,7 +1121,7 @@ static Bigint *d2b(U *d, int *e, int *bits, Stack_alloc *alloc)
     if ((k= lo0bits(&y)))
     {
       x[0]= y | z << (32 - k);
-      z>>= k;
+      z>>= (k == 32) ? (--k) : k;
     }
     else
       x[0]= y;
@@ -1906,7 +1906,8 @@ trimzeros:
     s++;
   }
 ret:
-  Bfree(S, &alloc);
+  if (S != NULL)
+    Bfree(S, &alloc);
   if (mhi)
   {
     if (mlo && mlo != mhi)

libmariadb/mariadb_lib.c
@@ -85,6 +85,7 @@ extern ulong net_buffer_length;  /* net.c */
 static MYSQL_PARAMETERS mariadb_internal_parameters= {&max_allowed_packet, &net_buffer_length, 0};
 static my_bool mysql_client_init=0;
 static void mysql_close_options(MYSQL *mysql);
+static void ma_clear_session_state(MYSQL *mysql);
 extern void release_configuration_dirs();
 extern char **get_default_configuration_dirs();
 extern my_bool  ma_init_done;
@@ -1906,6 +1907,7 @@ static void mysql_close_options(MYSQL *mysql)
 
 static void mysql_close_memory(MYSQL *mysql)
 {
+  ma_clear_session_state(mysql);
   free(mysql->host_info);
   free(mysql->host);
   free(mysql->user);
@@ -1925,11 +1927,23 @@ void my_set_error(MYSQL *mysql,
 {
   va_list ap;
 
+  const char *errmsg;
+
+  if (!format)
+  {
+    if (error_nr >= CR_MIN_ERROR && error_nr <= CR_MYSQL_LAST_ERROR)
+      errmsg= ER(error_nr);
+    else if (error_nr >= CER_MIN_ERROR && error_nr <= CR_MARIADB_LAST_ERROR)
+      errmsg= CER(error_nr);
+    else
+      errmsg= ER(CR_UNKNOWN_ERROR);
+  }
+
   mysql->net.last_errno= error_nr;
   ma_strmake(mysql->net.sqlstate, sqlstate, SQLSTATE_LENGTH);
   va_start(ap, format);
-  vsnprintf(mysql->net.last_error, MYSQL_ERRMSG_SIZE,
-            format ? format : ER(error_nr), ap);
+  vsnprintf(mysql->net.last_error, MYSQL_ERRMSG_SIZE - 1,
+            format ? format : errmsg, ap);
   va_end(ap);
   return;
 }
@@ -1947,7 +1961,7 @@ void mysql_close_slow_part(MYSQL *mysql)
   }
 }
 
-void ma_clear_session_state(MYSQL *mysql)
+static void ma_clear_session_state(MYSQL *mysql)
 {
   uint i;
 
@@ -1956,7 +1970,6 @@ void ma_clear_session_state(MYSQL *mysql)
 
   for (i= SESSION_TRACK_BEGIN; i <= SESSION_TRACK_END; i++)
   {
-    /* we acquired memory via ma_multi_alloc, so we don't need to free data */
     list_free(mysql->extension->session_state[i].list, 0);
   }
   memset(mysql->extension->session_state, 0, sizeof(struct st_mariadb_session_state) * SESSION_TRACK_TYPES);
@@ -2080,12 +2093,13 @@ int ma_read_ok_packet(MYSQL *mysql, uchar *pos, ulong length)
               if (si_type != SESSION_TRACK_STATE_CHANGE)
                 net_field_length(&pos); /* ignore total length, item length will follow next */
               plen= net_field_length(&pos);
-              if (!ma_multi_malloc(0,
+              if (!(session_item= ma_multi_malloc(0,
                                   &session_item, sizeof(LIST),
                                   &str, sizeof(MYSQL_LEX_STRING),
                                   &data, plen,
-                                  NULL))
+                                  NULL)))
               {
+                ma_clear_session_state(mysql);
                 SET_CLIENT_ERROR(mysql, CR_OUT_OF_MEMORY, SQLSTATE_UNKNOWN, 0);
                 return -1;
               }
@@ -2111,12 +2125,13 @@ int ma_read_ok_packet(MYSQL *mysql, uchar *pos, ulong length)
                 if (!strncmp(str->str, "character_set_client", str->length))
                   set_charset= 1;
                 plen= net_field_length(&pos);
-                if (!ma_multi_malloc(0,
+                if (!(session_item= ma_multi_malloc(0,
                                     &session_item, sizeof(LIST),
                                     &str, sizeof(MYSQL_LEX_STRING),
                                     &data, plen,
-                                    NULL))
+                                    NULL)))
                 {
+                  ma_clear_session_state(mysql);
                   SET_CLIENT_ERROR(mysql, CR_OUT_OF_MEMORY, SQLSTATE_UNKNOWN, 0);
                   return -1;
                 }
@@ -2172,7 +2187,7 @@ int mthd_my_read_query_result(MYSQL *mysql)
   if (mysql->options.extension && mysql->extension->auto_local_infile == ACCEPT_FILE_REQUEST)
     mysql->extension->auto_local_infile= WAIT_FOR_QUERY;
 
-  if (!mysql || (length = ma_net_safe_read(mysql)) == packet_error)
+  if ((length = ma_net_safe_read(mysql)) == packet_error)
   {
     return(1);
   }

libmariadb/mariadb_rpl.c
@@ -387,12 +387,14 @@ MARIADB_RPL_EVENT * STDCALL mariadb_rpl_fetch(MARIADB_RPL *rpl, MARIADB_RPL_EVEN
       }
       break;
     default:
+      free(rpl_event);
       return NULL;
       break;
     }
     return rpl_event;
   }
 mem_error:
+  free(rpl_event);
   SET_CLIENT_ERROR(rpl->mysql, CR_OUT_OF_MEMORY, SQLSTATE_UNKNOWN, 0);
   return 0;
 }
@@ -458,6 +460,7 @@ int STDCALL mariadb_rpl_optionsv(MARIADB_RPL *rpl,
     goto end;
   }
 end:
+  va_end(ap);
   return rc;
 }
 
@@ -501,8 +504,10 @@ int STDCALL mariadb_rpl_get_optionsv(MARIADB_RPL *rpl,
     break;
   }
   default:
+    va_end(ap);
     return 1;
     break;
   }
+  va_end(ap);
   return 0;
 }

plugins/auth/caching_sha2_pw.c
@@ -212,13 +212,15 @@ static char *load_pub_key_file(const char *filename, int *pub_key_size)
   if (fseek(fp, 0, SEEK_END))
     goto end;
 
-  *pub_key_size= ftell(fp);
+  if ((*pub_key_size= ftell(fp)) < 0)
+    goto end;
+
   rewind(fp);
 
   if (!(buffer= malloc(*pub_key_size + 1)))
     goto end;
 
-  if (!fread(buffer, *pub_key_size, 1, fp))
+  if (fread(buffer, *pub_key_size, 1, fp) != (size_t)*pub_key_size)
     goto end;
 
   error= 0;

plugins/auth/my_auth.c
@@ -118,7 +118,7 @@ static int dummy_fallback_auth_client(MYSQL_PLUGIN_VIO *vio, MYSQL *mysql __attr
   unsigned int i, last_errno= ((MCPVIO_EXT *)vio)->mysql->net.last_errno;
   if (last_errno)
     strncpy(last_error, ((MCPVIO_EXT *)vio)->mysql->net.last_error,
-            sizeof(last_error));
+            sizeof(last_error) - 1);
 
   /* safety-wise we only do 10 round-trips */
   for (i=0; i < 10; i++)
@@ -131,7 +131,7 @@ static int dummy_fallback_auth_client(MYSQL_PLUGIN_VIO *vio, MYSQL *mysql __attr
   }
   if (last_errno)
     strncpy(((MCPVIO_EXT *)vio)->mysql->net.last_error, last_error,
-            sizeof(((MCPVIO_EXT *)vio)->mysql->net.last_error));
+            sizeof(((MCPVIO_EXT *)vio)->mysql->net.last_error) - 1);
   return CR_ERROR;
 }
 

plugins/auth/old_password.c
@@ -83,7 +83,6 @@ static int auth_old_password(MYSQL_PLUGIN_VIO *vio, MYSQL *mysql)
       we use the old scramble.
     */
     pkt= (uchar*)mysql->scramble_buff;
-    pkt_len= SCRAMBLE_LENGTH_323 + 1;
   }
   else
   {

commit f16d2924adb4e59c772184c0ec6017c01995ad18

Fixed bug which was introduced in 2e804f0badce962057254841efae80715d2bf975:
Don't return a truncation error (101) in case length is larger than parameter offset.

libmariadb/ma_stmt_codec.c
@@ -596,8 +596,6 @@ static void convert_froma_string(MYSQL_BIND *r_param, char *buffer, size_t len)
         *r_param->error= (copylen > r_param->buffer_length);
 
       }
-      else
-        *r_param->error= 1;
       *r_param->length= (ulong)len;
     }
     break;