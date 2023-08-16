cpp-for-everything/mariadb-connector-c

commit b49f8d7c7e33b4ab11bfa401a489435b93fb6ebb

Fix for CONC-659:
When checking for a semi sync indication header, we need also check if
the undocumented session variable @rpl_semi_sync_slave was set.
Otherwise the timestamp of the event could contain values which match
the 2 bytes of the semi sync header.

Since the variable rpl_semi_sync_slave and it's behavior is not documented,
a new option MARIADB_RPL_SEMI_SYNC was added.

libmariadb/mariadb_rpl.c
@@ -33,6 +33,8 @@
 
 #ifdef WIN32
 #include <malloc.h>
+#undef alloca
+#define alloca _malloca
 #endif
 
 #define RPL_EVENT_HEADER_SIZE 19
@@ -690,9 +692,9 @@ MARIADB_RPL * STDCALL mariadb_rpl_init_ex(MYSQL *mysql, unsigned int version)
 
   if ((rpl->mysql= mysql))
   {
+    MYSQL_RES *result;
     if (!mysql_query(mysql, "select @@binlog_checksum"))
     {
-      MYSQL_RES *result;
       if ((result= mysql_store_result(mysql)))
       {
         MYSQL_ROW row= mysql_fetch_row(result);
@@ -795,6 +797,35 @@ int STDCALL mariadb_rpl_open(MARIADB_RPL *rpl)
     uint32_t replica_id= rpl->server_id;
     ptr= buf= (unsigned char *)alloca(rpl->filename_length + 11);
 
+    if (!ptr)
+    {
+      rpl_set_error(rpl, CR_OUT_OF_MEMORY, 0);
+    }
+
+    if (rpl->is_semi_sync)
+    {
+      if (mysql_query(rpl->mysql, "SET @rpl_semi_sync_slave=1"))
+      {
+        rpl_set_error(rpl, mysql_errno(rpl->mysql), 0, mysql_error(rpl->mysql));
+        return 1;
+      }
+    }
+    else {
+      MYSQL_RES* result;
+      MYSQL_ROW row;
+      if (mysql_query(rpl->mysql, "SELECT @rpl_semi_sync_slave=1"))
+      {
+        rpl_set_error(rpl, mysql_errno(rpl->mysql), 0, mysql_error(rpl->mysql));
+        return 1;
+      }
+      if ((result = mysql_store_result(rpl->mysql)))
+      {
+        if ((row = mysql_fetch_row(result)))
+          rpl->is_semi_sync = (row[0] != NULL && row[0][0] == '1');
+        mysql_free_result(result);
+      }
+    }
+
     int4store(ptr, (unsigned int)rpl->start_position);
     ptr+= 4;
     int2store(ptr, rpl->flags);
@@ -811,6 +842,9 @@ int STDCALL mariadb_rpl_open(MARIADB_RPL *rpl)
   {
     char *buf[RPL_BINLOG_MAGIC_SIZE];
     MYSQL mysql;
+
+    /* Semi sync doesn't work when processing files */
+    rpl->is_semi_sync = 0;
 
     if (rpl->fp)
       ma_close(rpl->fp);
@@ -907,6 +941,50 @@ static uint32_t get_compression_info(const unsigned char *buf,
 
   *header_size += 1;
   return len;
+}
+
+static uint8_t mariadb_rpl_send_semisync_ack(MARIADB_RPL* rpl, MARIADB_RPL_EVENT* event)
+{
+  size_t buf_size = 0;
+  uchar* buf;
+
+  if (!rpl)
+    return 1;
+
+  if (!event)
+  {
+    rpl_set_error(rpl, CR_BINLOG_SEMI_SYNC_ERROR, 0, "Invalid event");
+    return 1;
+  }
+
+  if (!rpl->is_semi_sync)
+  {
+    rpl_set_error(rpl, CR_BINLOG_SEMI_SYNC_ERROR, 0, "semi synchronous replication is not enabled");
+    return 1;
+  }
+  if (!event->is_semi_sync || !event->semi_sync_flags != SEMI_SYNC_ACK_REQ)
+  {
+    rpl_set_error(rpl, CR_BINLOG_SEMI_SYNC_ERROR, 0, "This event doesn't require to send semi synchronous acknoledgement");
+    return 1;
+  }
+
+  buf_size = rpl->filename_length + 9;
+  buf = alloca(buf_size);
+
+  buf[0] = SEMI_SYNC_INDICATOR;
+  int8store(buf + 1, event->next_event_pos);
+  memcpy(buf + 9, rpl->filename, rpl->filename_length);
+
+  ma_net_clear(&rpl->mysql->net);
+
+  if (ma_net_write(&rpl->mysql->net, buf, buf_size) ||
+    (ma_net_flush(&rpl->mysql->net)))
+  {
+    rpl_set_error(rpl, CR_CONNECTION_ERROR, 0);
+    return 1;
+  }
+
+  return 0;
 }
 
 MARIADB_RPL_EVENT * STDCALL mariadb_rpl_fetch(MARIADB_RPL *rpl, MARIADB_RPL_EVENT *event)
@@ -1032,7 +1110,7 @@ MARIADB_RPL_EVENT * STDCALL mariadb_rpl_fetch(MARIADB_RPL *rpl, MARIADB_RPL_EVEN
       rpl_event->ok= *ev++;
 
       /* CONC-470: add support for semi snychronous replication */
-      if ((rpl_event->is_semi_sync= (*ev == SEMI_SYNC_INDICATOR)))
+      if (rpl->is_semi_sync && (rpl_event->is_semi_sync= (*ev == SEMI_SYNC_INDICATOR)))
       {
         RPL_CHECK_POS(ev, ev_end, 1);
         ev++;
@@ -1826,19 +1904,11 @@ MARIADB_RPL_EVENT * STDCALL mariadb_rpl_fetch(MARIADB_RPL *rpl, MARIADB_RPL_EVEN
     if (rpl_event->is_semi_sync &&
         rpl_event->semi_sync_flags == SEMI_SYNC_ACK_REQ)
     {
-      size_t buf_size= rpl->filename_length + 1 + 8;
-      uchar *buffer= alloca(buf_size);
-
-      buffer[0]= SEMI_SYNC_INDICATOR;
-      int8store(buffer + 1, (int64_t)rpl_event->next_event_pos);
-      memcpy(buffer + 9, rpl->filename, rpl->filename_length);
-
-      /* clear network buffer before sending the reply packet*/
-      ma_net_clear(&rpl->mysql->net);
-
-      if (ma_net_write(&rpl->mysql->net, buffer, buf_size) ||
-         (ma_net_flush(&rpl->mysql->net)))
-        goto net_error;
+      if (mariadb_rpl_send_semisync_ack(rpl, rpl_event))
+      {
+        /* ACK failed and rpl->error was set */
+        return rpl_event;
+      }
     }
 
     if (rpl->use_checksum && !rpl_event->checksum)
@@ -1864,10 +1934,6 @@ MARIADB_RPL_EVENT * STDCALL mariadb_rpl_fetch(MARIADB_RPL *rpl, MARIADB_RPL_EVEN
 mem_error:
   mariadb_free_rpl_event(rpl_event);
   rpl_set_error(rpl, CR_OUT_OF_MEMORY, 0);
-  return 0;
-net_error:
-  mariadb_free_rpl_event(rpl_event);
-  rpl_set_error(rpl, CR_CONNECTION_ERROR, 0);
   return 0;
 malformed_packet:
   rpl_set_error(rpl, CR_BINLOG_ERROR, 0, RPL_ERR_POS(rpl),
@@ -1961,6 +2027,11 @@ int STDCALL mariadb_rpl_optionsv(MARIADB_RPL *rpl,
     rpl->extract_values= (uint8_t)va_arg(ap, uint32_t);
     break;
   }
+  case MARIADB_RPL_SEMI_SYNC:
+  {
+    rpl->is_semi_sync = (uint8_t)va_arg(ap, uint32_t);
+    break;
+  }
   default:
     rc= -1;
     goto end;
@@ -2009,6 +2080,12 @@ int STDCALL mariadb_rpl_get_optionsv(MARIADB_RPL *rpl,
     *start= rpl->start_position;
     break;
   }
+  case MARIADB_RPL_SEMI_SYNC:
+  {
+    unsigned int* semi_sync = va_arg(ap, unsigned int*);
+    *semi_sync = rpl->is_semi_sync;
+  }
+
   default:
     va_end(ap);
     return 1;

unittest/libmariadb/rpl_api.c
@@ -225,7 +225,6 @@ static int test_conc467(MYSQL *my __attribute__((unused)))
   mysql_query(mysql, "SET @slave_gtid_ignore_duplicates=1");
   mysql_query(mysql, "SET NAMES utf8");
   mysql_query(mysql, "SET @master_binlog_checksum= @@global.binlog_checksum");
-  mysql_query(mysql, "SET @rpl_semi_sync_slave=1");
   rpl->server_id= 12;
   rpl->start_position= 4;
   rpl->flags= MARIADB_RPL_BINLOG_SEND_ANNOTATE_ROWS;
@@ -343,9 +342,9 @@ static int test_conc592(MYSQL *my __attribute__((unused)))
 }
 
 struct my_tests_st my_tests[] = {
-  {"test_conc592", test_conc592, TEST_CONNECTION_NEW, 0, NULL, NULL},
-  {"test_rpl_async", test_rpl_async, TEST_CONNECTION_NEW, 0, NULL, NULL},
-  {"test_rpl_semisync", test_rpl_semisync, TEST_CONNECTION_NEW, 0, NULL, NULL},
+ // {"test_conc592", test_conc592, TEST_CONNECTION_NEW, 0, NULL, NULL},
+  //{"test_rpl_async", test_rpl_async, TEST_CONNECTION_NEW, 0, NULL, NULL},
+  //{"test_rpl_semisync", test_rpl_semisync, TEST_CONNECTION_NEW, 0, NULL, NULL},
   {"test_conc467", test_conc467, TEST_CONNECTION_NEW, 0, NULL, NULL},
   {NULL, NULL, 0, 0, NULL, NULL}
 };

include/errmsg.h
@@ -113,6 +113,7 @@ extern const char *mariadb_client_errors[];	/* Error messages */
 #define CR_UNKNOWN_BINLOG_EVENT 5020
 #define CR_BINLOG_ERROR 5021
 #define CR_BINLOG_INVALID_FILE 5022
+#define CR_BINLOG_SEMI_SYNC_ERROR 5023
 
 /* Always last, if you add new error codes please update the
    value for CR_MARIADB_LAST_ERROR */

include/mariadb_rpl.h
@@ -86,7 +86,8 @@ enum mariadb_rpl_option {
   MARIADB_RPL_UNCOMPRESS,
   MARIADB_RPL_HOST,
   MARIADB_RPL_PORT,
-  MARIADB_RPL_EXTRACT_VALUES
+  MARIADB_RPL_EXTRACT_VALUES,
+  MARIADB_RPL_SEMI_SYNC,
 };
 
 /* Event types: From MariaDB Server sql/log_event.h */
@@ -340,7 +341,8 @@ typedef struct st_mariadb_rpl {
   uint8_t extract_values;
   char nonce[12];
   uint8_t encrypted;
-} MARIADB_RPL;
+  uint8_t is_semi_sync;
+}MARIADB_RPL;
 
 typedef struct st_mariadb_rpl_value {
   enum enum_field_types field_type;

libmariadb/ma_errmsg.c
@@ -117,6 +117,7 @@ const char *mariadb_client_errors[] =
   /* 5020 */ "Binary log error (File: %.*s start_pos=%ld): Unknown event type (%d) with flag 'not_ignorable'.",
   /* 5021 */ "Binary log error (File: %.*s start_pos=%ld): %s.",
   /* 5022 */ "File '%s' is not a binary log file",
+  /* 5023 */ "Semi sync request error: %s",
   ""
 };
 

commit ba60805633d295315546a19737c2c1634c504eaa

Build fixes:
- Fixed build error, which was previously introduced by commit
b49f8d7c7e33b4ab11bfa401a489435b93fb6ebb

- Treat warnings as errors

CMakeLists.txt
@@ -99,8 +99,6 @@ IF(MSVC)
   INCLUDE(${CC_SOURCE_DIR}/cmake/WindowsCache.cmake)
   ADD_DEFINITIONS(-DWIN32_LEAN_AND_MEAN -DNOGDI)
   IF (MSVC)
-    # Treat warnings as errors
-    SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -WX")
     SET(CONFIG_TYPES "DEBUG" "RELEASE" "RELWITHDEBINFO")
     FOREACH(BUILD_TYPE ${CONFIG_TYPES})
       FOREACH(COMPILER CXX C)
@@ -143,6 +141,13 @@ IF(CMAKE_COMPILER_IS_GNUCC)
     ENDIF()
   ENDFOREACH()
 ENDIF()
+
+IF (CMAKE_C_COMPILER_ID MATCHES "Clang" OR CMAKE_C_COMPILER_ID MATCHES "GNU")
+  SET(WARNING_AS_ERROR "-Werror" CACHE INTERNAL "WARNING_AS_ERROR")
+ELSEIF(CMAKE_C_COMPILER_ID MATCHES "MSVC")
+  SET(WARNING_AS_ERROR "/WX" CACHE INTERNAL "WARNING_AS_ERROR")
+ENDIF()
+SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${WARNING_AS_ERROR}")
 
 # If the build type isn't specified, set to Relwithdebinfo as default.
 IF(NOT CMAKE_BUILD_TYPE)

external/zlib/CMakeLists.txt
@@ -5,6 +5,9 @@ project(zlib C)
 
 set(VERSION "1.2.13")
 
+# Don't treat warnings as error for zlib
+STRING(REPLACE ${WARNING_AS_ERROR} "" CMAKE_C_FLAGS ${CMAKE_C_FLAGS})
+
 set(INSTALL_BIN_DIR "${CMAKE_INSTALL_PREFIX}/bin" CACHE PATH "Installation directory for executables")
 set(INSTALL_LIB_DIR "${CMAKE_INSTALL_PREFIX}/lib" CACHE PATH "Installation directory for libraries")
 set(INSTALL_INC_DIR "${CMAKE_INSTALL_PREFIX}/include" CACHE PATH "Installation directory for headers")

include/ma_global.h
@@ -27,6 +27,7 @@
 #include <stdlib.h>
 #define strcasecmp _stricmp
 #define strtok_r strtok_s
+#define strdup _strdup
 #define sleep(x) Sleep(1000*(x))
 #ifdef _MSC_VER
 #define inline __inline

libmariadb/ma_default.c
@@ -29,6 +29,8 @@
 #include <io.h>
 #include "shlwapi.h"
 
+#define access _access
+
 static const char *ini_exts[]= {"ini", "cnf", 0};
 #define R_OK 4
 #else

libmariadb/mariadb_rpl.c
@@ -962,7 +962,7 @@ static uint8_t mariadb_rpl_send_semisync_ack(MARIADB_RPL* rpl, MARIADB_RPL_EVENT
     rpl_set_error(rpl, CR_BINLOG_SEMI_SYNC_ERROR, 0, "semi synchronous replication is not enabled");
     return 1;
   }
-  if (!event->is_semi_sync || !event->semi_sync_flags != SEMI_SYNC_ACK_REQ)
+  if (!event->is_semi_sync || (event->semi_sync_flags != SEMI_SYNC_ACK_REQ))
   {
     rpl_set_error(rpl, CR_BINLOG_SEMI_SYNC_ERROR, 0, "This event doesn't require to send semi synchronous acknoledgement");
     return 1;
@@ -972,7 +972,7 @@ static uint8_t mariadb_rpl_send_semisync_ack(MARIADB_RPL* rpl, MARIADB_RPL_EVENT
   buf = alloca(buf_size);
 
   buf[0] = SEMI_SYNC_INDICATOR;
-  int8store(buf + 1, event->next_event_pos);
+  int8store(buf + 1, (uint64_t)event->next_event_pos);
   memcpy(buf + 9, rpl->filename, rpl->filename_length);
 
   ma_net_clear(&rpl->mysql->net);
@@ -2084,6 +2084,7 @@ int STDCALL mariadb_rpl_get_optionsv(MARIADB_RPL *rpl,
   {
     unsigned int* semi_sync = va_arg(ap, unsigned int*);
     *semi_sync = rpl->is_semi_sync;
+    break;
   }
 
   default:

unittest/libmariadb/ma_getopt.h
@@ -121,11 +121,7 @@ extern int _getopt_internal (int argc, char *const *argv,
 			     const struct option *longopts, int *longind,
 			     int long_only);
 #else /* not __STDC__ */
-extern int getopt ();
-extern int getopt_long ();
-extern int getopt_long_only ();
-
-extern int _getopt_internal ();
+extern int getopt (int argc, char *const *argv, const char *optstring);
 #endif /* __STDC__ */
 
 #ifdef	__cplusplus

unittest/libmariadb/my_test.h
@@ -38,6 +38,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include <pthread.h>
 #else
 #include <io.h>
+#define unlink _unlink
 #endif
 
 #ifndef OK

unittest/libmariadb/rpl_api.c
@@ -342,9 +342,9 @@ static int test_conc592(MYSQL *my __attribute__((unused)))
 }
 
 struct my_tests_st my_tests[] = {
- // {"test_conc592", test_conc592, TEST_CONNECTION_NEW, 0, NULL, NULL},
-  //{"test_rpl_async", test_rpl_async, TEST_CONNECTION_NEW, 0, NULL, NULL},
-  //{"test_rpl_semisync", test_rpl_semisync, TEST_CONNECTION_NEW, 0, NULL, NULL},
+  {"test_conc592", test_conc592, TEST_CONNECTION_NEW, 0, NULL, NULL},
+  {"test_rpl_async", test_rpl_async, TEST_CONNECTION_NEW, 0, NULL, NULL},
+  {"test_rpl_semisync", test_rpl_semisync, TEST_CONNECTION_NEW, 0, NULL, NULL},
   {"test_conc467", test_conc467, TEST_CONNECTION_NEW, 0, NULL, NULL},
   {NULL, NULL, 0, 0, NULL, NULL}
 };